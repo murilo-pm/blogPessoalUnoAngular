d7b4ce23830baad62d902ba866ab66e4a66e7d69
Murilo <murilopaulino@live.com>
2021-05-06 15:43:51 -0300

    criação dos métodos PUT e DELETE de Tema

---
+++ src/app/edit/tema-edit/tema-edit.component.ts
@@ -0,0 +1,58 @@
+import { Route } from '@angular/compiler/src/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tema } from 'src/app/model/Tema';
+import { TemaService } from 'src/app/service/tema.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-tema-edit',
+  templateUrl: './tema-edit.component.html',
+  styleUrls: ['./tema-edit.component.css']
+})
+export class TemaEditComponent implements OnInit {
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[];
+
+  constructor(
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(){   
+    if(environment.token==""){
+      this.router.navigate(["/entrar"])
+    }
+  
+    /* if(environment.token != ""){
+      this.findAllTema()
+    } */
+
+     let id = this.route.snapshot.params["id"]
+    this.findByIdTema(id) 
+
+  }
+
+  findByIdTema(id:number){
+    this.temaService.getByIdTema(id).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  atualizar(){
+    this.temaService.putTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert("tema atualizado")
+      this.router.navigate(["/tema"])
+    })
+  }
+
+  /* findAllTema(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  } */
+
+}
